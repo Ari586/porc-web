54b779e281fb1347f58231c2f64e3412f020d46f
Ari586 <la7ariel@gmail.com>
2026-02-22 18:52:28 +0100

    Correct web build with base-href /porc-web/

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2655815507"
+    serviceWorkerVersion: "1699941744"
   }
 });
